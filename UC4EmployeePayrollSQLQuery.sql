830b41559913c8a2520733958509cc0c433cdda2
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-03-23 23:29:00 +0530

    [Anuj]Add: Retrieve all data added in Payroll service

---
+++ UC4EmployeePayrollSQLQuery.sql
@@ -0,0 +1,21 @@
+--UC-1
+create database PayRoll_Service;
+
+--UC-2
+create Table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(200) not null,
+salary float,
+startDate date
+)
+--UC-3
+Insert into employee_payroll(name,salary,startDate) values
+('Anuj',36000,'2022-12-12'),
+('Rahul',47000,'2022-08-18'),
+('Amit',45000,'2021-08-21'),
+('Kamal',35000,'2021-08-19');
+
+--UC-4
+select *from employee_payroll;
+
